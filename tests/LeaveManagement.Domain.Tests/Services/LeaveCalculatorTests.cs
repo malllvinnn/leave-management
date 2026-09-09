23dafaddabc60729e731caffc07d64f442c94a75
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-09-10 03:04:52 +0700

    test(domain): define leave calculator input guards

--- tests/LeaveManagement.Domain.Tests/Services/LeaveCalculatorTests.cs
+++ tests/LeaveManagement.Domain.Tests/Services/LeaveCalculatorTests.cs
@@ -1,4 +1,5 @@
 using LeaveManagement.Domain.Services;
+using LeaveManagement.Domain.ValueObjects;
 
 namespace LeaveManagement.Domain.Tests.Services;
 
@@ -11,5 +12,43 @@ public class LeaveCalculatorTests
     public void Setup()
     {
         _leaveCalculator = new LeaveCalculator();
+    }
+
+    [Test]
+    public void Calculate_WithNullPeriod_ThrowsArgumentNullExceptionWithPeriodParamName()
+    {
+        // Arrange
+        DateRange period = null!;
+        var holidays = Array.Empty<DateOnly>();
+
+        // Act
+        var exception = Assert.Throws<ArgumentNullException>(() => _leaveCalculator.Calculate(
+            period: period,
+            holidays: holidays
+        ));
+
+        // Assert
+        Assert.That(exception.ParamName, Is.EqualTo("period"));
+    }
+
+    [Test]
+    public void Calculate_WithNullHolidays_ThrowsArgumentNullExceptionWithHolidaysParamName()
+    {
+        // Arrange
+        var period = DateRange.Create(
+            start: new DateOnly(2026, 12, 21),
+            end: new DateOnly(2026, 12, 25)
+        ).Value;
+
+        IReadOnlyCollection<DateOnly> holidays = null!;
+
+        // Act
+        var exception = Assert.Throws<ArgumentNullException>(() => _leaveCalculator.Calculate(
+            period: period,
+            holidays: holidays
+        ));
+
+        // Assert
+        Assert.That(exception.ParamName, Is.EqualTo("holidays"));
     }
 }
